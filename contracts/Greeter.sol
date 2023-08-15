83ad627fc5cb436a3de4fcf7a35f4632e5cd9669
subaru-hello <octpsubaru@icloud.com>
2023-08-15 21:53:45 +0900

    add owner function

--- contracts/Greeter.sol
+++ contracts/Greeter.sol
@@ -2,12 +2,29 @@ pragma solidity >= 0.4.0 < 0.7.0;
 
 contract Greeter {
 	string private _greeting = "Hello, World!";
+	address private _owner;
+
+	modifier onlyOwner() {
+		require(
+			msg.sender == _owner,
+			"Ownable: caller is not the owner"
+		);
+		_;
+	}
+
+	constructor() public {
+		_owner = msg.sender;
+	}
 
 	function greet() external view returns(string memory) {
 		return _greeting;
 	}
 
-	function setGreeting(string calldata greeting) external {
+	function setGreeting(string calldata greeting) external onlyOwner {
 		_greeting = greeting;
+	}
+
+	function owner() public view returns(address) {
+		return _owner;
 	}
 }
